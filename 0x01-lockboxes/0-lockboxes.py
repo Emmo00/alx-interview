51a12fd31bcb5f0a0e199f731994b62d5a57d026
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-08 15:30:45 +0100

    brute force

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+"""lockboxes"""
+
+
+def extract_keys(box_keys, boxes):
+    """extract keys from boxes"""
+    found = list(box_keys)
+    for key in found:
+        if key > len(boxes):
+            continue
+        for i in boxes[key]:
+            if i not in found:
+                found.append(i)
+    return found
+
 
 def canUnlockAll(boxes: list[list[int]]):
-    pass
+    """checks if a list of boxes can all be unlocked"""
+    keys = []
+    locked_boxes = [i for i in range(1, len(boxes))]
+    keys.extend(extract_keys(boxes[0], boxes))
+    keys = set(keys)
+    for box in locked_boxes:
+        if box not in keys:
+            return False
+    return True
