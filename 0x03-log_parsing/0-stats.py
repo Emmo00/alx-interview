3a3aa8a2aa7c336de9d52244ebb50ca8e3f80f3b
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-19 13:24:13 -0700

    change_up

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,10 +6,12 @@
 def print_stats(filesize, status_stats):
     """prints stats collected
     """
-    print("File size: {}".format(filesize))
+    stats = ""
+    stats += "File size: {}\n".format(filesize)
     for code in sorted(status_stats):
         if status_stats[code] > 0:
-            print("{}: {}".format(code, status_stats[code]))
+            stats += "{}: {}\n".format(code, status_stats[code])
+    print(stats[:-1])
 
 
 if __name__ == '__main__':
@@ -23,16 +25,15 @@ if __name__ == '__main__':
     i = 1
     try:
         for line in sys.stdin:
-            if i % 10 == 0:
-                print_stats(filesize, status_codes)
-                i = 1
-            else:
-                i += 1
             data_list = line.split(" ")
             status_in = data_list[-2]
             filesize_in = data_list[-1]
             if int(status_in) in status_codes:
                 status_codes[int(status_in)] += 1
             filesize += int(filesize_in)
+            if i % 10 == 0:
+                print_stats(filesize, status_codes)
+            i += 1
     except KeyboardInterrupt as ki:
         print_stats(filesize, status_codes)
+        raise ki
