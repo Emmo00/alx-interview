2e1012d6d865b0e67d27dbd8b52ed43d6e3d4f72
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-24 12:11:14 +0100

    cp

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,14 +5,21 @@ validate utf8
 
 
 def validUTF8(data):
-    try:
-        for i in range(len(data)):
-            if data[i] >= 192:
-                if data[i + 1] >= 192:
-                    return False
-                if data[i] >= 224:
-                    if data[i + 2] >= 192:
-                        return False
-    except IndexError:
-        return False
-    return True
+    bytes_to_check = 0
+    for byte in data:
+        if bytes_to_check == 0:
+            if (byte & 0x80) == 0:
+                bytes_to_check = 0
+            elif (byte & 0xE0) == 0xC0:
+                bytes_to_check = 1
+            elif (byte & 0xF0) == 0xE0:
+                bytes_to_check = 2
+            elif (byte & 0xF8) == 0xF0:
+                bytes_to_check = 3
+            else:
+                return False
+        else:
+            if (byte & 0xC0) != 0x80:
+                return False
+            bytes_to_check -= 1
+    return bytes_to_check == 0
