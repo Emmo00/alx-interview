464abb84d1b10f2fa4215990961e4c5969dd55c9
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-06 08:06:23 +0100

    min abeg

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+minimum operations
+"""
+from typing import Tuple
+
+
+def factors(n: int) -> Tuple[int, int]:
+    """get min and max factors of a number"""
+    if n == 1:
+        return 1, 1
+    if n == 0:
+        return 0, 0
+    for i in range(2, int(n / 2) + 1):
+        if n / i == n // i:
+            return i, int(n / i)
+    return 1, n
+
+
+def minOperations(n: int) -> int:
+    """
+    calculate the minimum operations
+    """
+    min_factors, max_factors = factors(n)
+    if min_factors == 1:
+        return max_factors
+    if min_factors == 0:
+        return 0
+    return min_factors + minOperations(max_factors)
