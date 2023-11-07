bf37f4440229f91c56e3cf882af19d6808b56fd3
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-07 21:58:30 +0100

    m

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,12 +2,13 @@
 const request = require("request");
 const movieId = process.argv[2];
 const apiURL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-request.get(apiURL, (err, res, data) => {
+request.get(apiURL, async (err, res, data) => {
   if (err) console.log(err);
   for (const character of JSON.parse(data).characters) {
     request.get(character, (error, res, data) => {
       if (error) console.log(error);
       console.log(JSON.parse(data).name);
     });
+    await new Promise((resolve) => setTimeout(resolve, 200));
   }
 });
