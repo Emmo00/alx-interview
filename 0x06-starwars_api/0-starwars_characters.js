c94805cdf0e72616ee0eb3b99adecd713612245b
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-07 21:27:11 +0100

    d

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require("request");
+
+const filmId = process.argv[2];
+
+request(
+  `https://swapi-api.alx-tools.com/api/films/${filmId}`,
+  function (error, response, body) {
+    if (error) console.error(error);
+
+    const data = JSON.parse(body);
+    characterIds = data.characters.map((url) => Number(url.split("/").at(-2)));
+    characterIds
+      .sort((a, b) => a - b)
+      .forEach((id) =>
+        request(
+          `https://swapi-api.alx-tools.com/api/people/${id}`,
+          (error, res, character) => {
+            console.log(characterIds)
+            if (error) console.error(error);
+            console.log(JSON.parse(character).name);
+          }
+        )
+      );
+  }
+);
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,3 @@
+alx project
+
+starwars api
