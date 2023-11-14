7071225435c0c9ce4a9964210a46ea9a28db9ac8
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-14 09:57:00 -0800

    j

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,8 +7,7 @@ def rotate_2d_matrix(matrix):
     """rotate 2d matrix
     """
     rows = len(matrix)
-    rotated: set[list] = set()
-
+    rotated = set()
     for i in range(rows):
         for j in range(rows):
             if not((i, j) in rotated or (j, i) in rotated):
