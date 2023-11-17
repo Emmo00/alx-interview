536d5c74f4e7044101556e6c8616f06aa7614a57
Emmanuel Nwafor <88244468+Emmo00@users.noreply.github.com>
2023-11-17 09:30:50 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,7 +10,7 @@ def rotate_2d_matrix(matrix):
     rotated = set()
     for i in range(rows):
         for j in range(rows):
-            if not((i, j) in rotated or (j, i) in rotated):
+            if not ((i, j) in rotated or (j, i) in rotated):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
                 rotated.add((i, j))
                 rotated.add((j, i))
