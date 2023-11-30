8783077004f72ecc59862a34cda3efd78c109a52
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-11-30 17:13:07 +0100

    check

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,6 +7,8 @@ def island_perimeter(grid):
     """island perimeter
     """
     perimeter = 0
+    if grid is None or len(grid) == 0 or len(grid[0]) == 0:
+        return 0
     for i in range(len(grid)):
         start = True
         for j in range(len(grid[i])):
