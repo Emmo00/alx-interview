36fb1acd658b247b7b10c7e7844481c0dba60546
Emmanuel Nwafor@ubuntuVM <nwaforemmanuel005@gmail.com>
2023-12-06 05:54:49 +0100

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""module
+"""
+def isPrime(number):
+    limit = number // 2
+    for i in range(limit):
+        if number / i == number // i:
+            return False
+    return True
+
+
+def lowest_prime_number(numbers):
+    sorted_numbers = sorted(numbers)
+    for i in sorted_numbers:
+        if isPrime(i):
+            return i
+
+
+def toggle_turn(current_player):
+    return 'Ben' if current_player == 'Maria' else 'Maria'
+
+
+def winner(players: dict):
+    keys = list(players.keys())
+    values = list(players.values())
+    max_player = max(values)
+    winner_index = values.index(max_player)
+    return keys[winner_index]
+
+
+def isWinner(x, nums):
+    """prime game
+    """
+    players = {
+        'Maria': 0,
+        'Ben': 0,
+    }
+    for round in range(x):
+        turn = 'Maria'
+        numbers = list(range(1, round + 1))
+        try:
+            choice = lowest_prime_number(numbers)
+            # remove multiples
+            j = 1
+            while j < max(numbers):
+                if choice * j in numbers:
+                    numbers.remove(choice * j)
+                j += 1
+            turn = toggle_turn(turn)
+        except:
+            # other person wins round
+            turn = toggle_turn(turn)
+            players[turn] += 1
+            continue
+    return winner(players)
